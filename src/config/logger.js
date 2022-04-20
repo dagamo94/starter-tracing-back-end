994dfdae16316ef03145aa0fbce67fb786319636
dan <da.ga.mo.94@gmail.com>
2022-04-20 15:40:06 -0600

    add pretty print to development messages

--- src/config/logger.js
+++ src/config/logger.js
@@ -3,9 +3,13 @@ const { nanoid } = require("nanoid");
 
 const level = process.env.LOG_LEVEL || "info"; // use LOG_LEVEL to set the log level of the logger or set it as 'info' as default
 
+const nodeEnv = process.env.NODE_ENV || 'development';
+const prettyPrint = nodeEnv === "development";
+
 const logger = pinoHttp({
   genReqId: (request) => request.headers['x-request-id'] || nanoid(),
-  level
+  level,
+  prettyPrint
 });
 
 module.exports = logger;
